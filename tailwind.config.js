c962e94a78ddec0ad8f7d4bdefbec36e9b64c94e
Edgar Herrera <edgarh09lfc@gmail.com>
2023-07-21 02:18:20 -0700

    Fix responsive title

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./views/**/*.ejs'],
   theme: {
     extend: {
+      width: {
+      '120': '120px',
+       },
+       colors: {
+        'rest': '#A41629'
+       },
+       backgroundImage: {
+       'mobile-back': "linear-gradient( rgba(255, 255, 255, 0.5), rgba(252, 250, 250, 0.3)), url('../images/toast.jpg')",
+       },
     },
     fontFamily: {
       'nunito': ['"Nunito"'],
